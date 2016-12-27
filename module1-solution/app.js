39276f37215d70a6f22c5fc270795e979cacb870
anantajitjg <anantajitjg2010@gmail.com>
2016-12-27 17:46:23 +0530

    Code fixes and improvements

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,32 +7,32 @@
     function LunchCheckController($scope) {
         $scope.lunchList = "";
         $scope.message = "";
-        $scope.helpMessage="";
+        $scope.helpMessage = "";
+
+        function setAlertStyle(color) {
+            $scope.inputStyle = {
+                'border-color': color
+            };
+            $scope.mesageStyle = {
+                'color': color
+            };
+        }
         $scope.checkIfTooMuch = function() {
             var lunchList = $scope.lunchList;
             if (lunchList.length > 0) {
-                var count=0;
+                var count = 0;
                 var lunchArr = lunchList.split(",");
-                var lunchArrSize=lunchArr.length;
-                for(var i=0;i<lunchArrSize;i++){
-                  if(lunchArr[i].trim().length>0){
-                    count++;
-                  }
+                var lunchArrSize = lunchArr.length;
+                for (var i = 0; i < lunchArrSize; i++) {
+                    if (lunchArr[i].trim().length > 0) {
+                        count++;
+                    }
                 }
-                $scope.helpMessage="";
-                $scope.inputStyle={'border-color':'#398439'};
-                $scope.mesageStyle={'color':'#398439'};
-                if (count > 3) {
-                    $scope.message = "Too much!";
-                } else {
-                    $scope.message = "Enjoy!";
-                }
-                if(count<lunchArrSize){
-                  $scope.helpMessage="I don't consider empty item!";
-                }
+                setAlertStyle('#398439');
+                $scope.message = count > 3 ? "Too much!" : "Enjoy!";
+                $scope.helpMessage = count < lunchArrSize ? "I don't consider empty item!" : "";
             } else {
-                $scope.inputStyle={'border-color':'#B5555A'};
-                $scope.mesageStyle={'color':'#B5555A'};
+                setAlertStyle('#B5555A');
                 $scope.message = "Please enter data first";
             }
         };
